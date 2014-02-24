2bd52aec069a6a9a6d74c8bfbbab198e15a63140
liangjun.zhong <zhongliangjun1@gmail.com>
2014-02-24 21:48:50 +0800

    add test

---
+++ src/test/java/com/dianping/Test.java
@@ -0,0 +1,11 @@
+package com.dianping;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: liangjun.zhong
+ * Date: 14-2-24
+ * Time: PM9:46
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
