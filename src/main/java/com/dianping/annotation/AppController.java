c2e6e2708cbe4c4542f412d6eff3d99fd05f234a
liangjun.zhong <zhongliangjun1@gmail.com>
2014-04-09 09:48:21 +0800

    spring mvc demo

---
+++ src/main/java/com/dianping/annotation/AppController.java
@@ -0,0 +1,31 @@
+package com.dianping.annotation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: liangjun.zhong
+ * Date: 14-4-8
+ * Time: PM3:55
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Controller
+//@RequestMapping("/spring-mvc")
+public class AppController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String hello(@RequestParam(value = "name", required = false, defaultValue = "soso") String name,  ModelMap model){
+
+        model.put("isOk", true);
+        model.put("message", "hello "+name);
+
+        return "hello";
+    }
+
+
+}
---
+++ src/main/webapp/WEB-INF/pages/hello.ftl
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <#--<script src="/js/jquery.js"></script>-->
+    </head>
+    <body>
+        <#if isOk >
+        <div>${message}</div>
+        </#if>
+    </body>
+</html>
