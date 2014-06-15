084f71394d5586440c805808cf41625a97f5679d
liangjun.zhong <zhongliangjun1@gmail.com>
2014-06-16 00:57:05 +0800

    add constructor.newInstance test

--- src/main/java/com/dianping/reflect/MyImplements.java
+++ src/main/java/com/dianping/reflect/MyImplements.java
@@ -1,5 +1,9 @@
 package com.dianping.reflect;
 
+import groovy.lang.GroovyClassLoader;
+
+import java.lang.reflect.Constructor;
+
 /**
  * Created with IntelliJ IDEA.
  * Author: liangjun.zhong
@@ -28,11 +32,33 @@ public class MyImplements implements InterfaceC {
         //To change body of implemented methods use File | Settings | File Templates.
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Class<?>[] interfaces = MyImplements.class.getInterfaces();
         for(Class clazz : interfaces){
             System.out.println(clazz.getName());
         }
+
+        String s = null;
+        try {
+            s.charAt(0);
+        } catch (Exception e) {
+            Constructor constructor = TemplateException.class.getConstructor(Throwable.class);
+            RuntimeException runtimeException = (RuntimeException) constructor.newInstance(e);
+            //runtimeException.printStackTrace();
+            System.out.println(runtimeException);
+        }
+
+        GroovyClassLoader loader = new GroovyClassLoader();
+        Class cla = loader.parseClass(templateException.replaceAll("#widgetName#", "Test"));
+        System.out.println(cla);
+
     }
+
+    private static final String templateException = "" +
+            "public class #widgetName#Exception extends RuntimeException {\n" +
+            "    public #widgetName#Exception(Throwable cause) {\n" +
+            "        super(cause);\n" +
+            "    }\n" +
+            "}";
 
 }
---
+++ src/main/java/com/dianping/reflect/TemplateException.java
@@ -0,0 +1,15 @@
+package com.dianping.reflect;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: liangjun.zhong
+ * Date: 14-6-12
+ * Time: AM2:04
+ * To change this template use File | Settings | File Templates.
+ */
+public class TemplateException extends RuntimeException {
+
+    public TemplateException(Throwable cause) {
+        super(cause);
+    }
+}
