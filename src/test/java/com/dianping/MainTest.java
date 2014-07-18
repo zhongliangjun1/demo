460a7e06594dc0c1ff016668f343bcc780594321
liangjun.zhong <zhongliangjun1@gmail.com>
2014-07-18 19:23:42 +0800

    fix bug

---
+++ src/test/java/com/dianping/MainTest.java
@@ -0,0 +1,17 @@
+package com.dianping;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: liangjun.zhong
+ * Date: 14-6-18
+ * Time: PM4:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainTest {
+
+    public static void main(String[] args) {
+        Integer i = 0;
+        System.out.println(i+"");
+    }
+
+}
