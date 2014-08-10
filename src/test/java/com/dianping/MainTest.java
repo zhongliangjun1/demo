1d621c077182d7eec78d3c12786d7b60b52acb53
liangjun.zhong <zhongliangjun1@gmail.com>
2014-08-10 09:45:52 +0800

    test

--- src/test/java/com/dianping/MainTest.java
+++ src/test/java/com/dianping/MainTest.java
@@ -1,5 +1,12 @@
 package com.dianping;
 
+import org.apache.http.client.fluent.Content;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.client.fluent.Response;
+
+import java.io.IOException;
+import java.security.Security;
+
 /**
  * Created with IntelliJ IDEA.
  * Author: liangjun.zhong
@@ -9,9 +16,14 @@ package com.dianping;
  */
 public class MainTest {
 
-    public static void main(String[] args) {
-        Integer i = 0;
-        System.out.println(i+"");
+    public static void main(String[] args) throws IOException {
+        System.setProperty("javax.net.ssl.trustStore", "/Library/Java/Home/lib/security/cacerts");
+        System.setProperty("java.protocol.handler.pkgs","com.sun.net.ssl.internal.www.protocol");
+        Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+
+        Response response = Request.Get("https://graph.qq.com/v3/qqclient/dianping_reddot?appid=200002&openid=86C72E87D7DD5A979FDE5AB2CA904907&access_token=BACBB4CB8BF92D5B51A2C05A4626E894&pf=qzone").execute();
+        Content content = response.returnContent();
+        System.out.println(content);
     }
 
 }
