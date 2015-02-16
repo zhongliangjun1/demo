12f31deeb7df763f8801a728f885e318443c440e
liangjun.zhong <zhongliangjun1@gmail.com>
2015-02-16 16:47:22 +0800

    get info from list bean

---
+++ src/main/java/com/dianping/start/ListBean.java
@@ -0,0 +1,23 @@
+package com.dianping.start;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: liangjun.zhong
+ * Date: 15/2/16
+ * Time: PM4:31
+ * To change this template use File | Settings | File Templates.
+ */
+public class ListBean {
+
+    private List<String> names;
+
+    public List<String> getNames() {
+        return names;
+    }
+
+    public void setNames(List<String> names) {
+        this.names = names;
+    }
+}
--- src/main/java/com/dianping/start/MyBeanFactoryPostProcessor.java
+++ src/main/java/com/dianping/start/MyBeanFactoryPostProcessor.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.config.TypedStringValue;
+import org.springframework.beans.factory.support.ManagedList;
 
 /**
  * Created with IntelliJ IDEA.
@@ -20,6 +21,7 @@ public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
         System.out.println("postProcessBeanFactory---------begin");
+
         System.out.println("decide to process myBean");
         BeanDefinition myBeanDefinition = configurableListableBeanFactory.getBeanDefinition("myBean");
         if(myBeanDefinition!=null){
@@ -31,6 +33,15 @@ public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
             String strValue = value.getValue();
             propertyValue.setConvertedValue("guess ni mei");
             System.out.println("modify myBean sex");
+        }
+
+        System.out.println("get info about listBean");
+        BeanDefinition listBeanDefinition = configurableListableBeanFactory.getBeanDefinition("listBean");
+        if ( listBeanDefinition!=null ) {
+            MutablePropertyValues propertyValues = listBeanDefinition.getPropertyValues();
+            PropertyValue propertyValue = propertyValues.getPropertyValue("names");
+            ManagedList<TypedStringValue> managedList = (ManagedList<TypedStringValue>) propertyValue.getValue();
+            System.out.println(managedList.get(0).getValue());
         }
 
         String[] beanDefinitionNames = configurableListableBeanFactory.getBeanDefinitionNames();
