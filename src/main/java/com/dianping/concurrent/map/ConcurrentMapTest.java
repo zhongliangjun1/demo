67cbe8915dc7edc4d49b774d61918e3b6f9b5019
liangjun.zhong <zhongliangjun1@gmail.com>
2015-03-10 21:36:36 +0800

    add ConcurrentMapTest

---
+++ src/main/java/com/dianping/concurrent/map/ConcurrentMapTest.java
@@ -0,0 +1,28 @@
+package com.dianping.concurrent.map;
+
+import com.google.common.collect.Maps;
+
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: liangjun.zhong
+ * Date: 15/3/9
+ * Time: PM4:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class ConcurrentMapTest {
+
+    public static void concurrentMapOfGuavaTest() {
+
+        Map<String, Object> data = Maps.newConcurrentMap();
+
+        data.put("null", null);
+
+    }
+
+    public static void main(String[] args) {
+        concurrentMapOfGuavaTest();
+    }
+
+}
