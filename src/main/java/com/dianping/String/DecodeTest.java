7a887ce661a884820adc9c70e3bec1c19749c838
liangjun.zhong <zhongliangjun1@gmail.com>
2015-03-18 22:13:14 +0800

    encode

--- src/main/java/com/dianping/String/DecodeTest.java
+++ src/main/java/com/dianping/String/DecodeTest.java
@@ -1,6 +1,7 @@
 package com.dianping.String;
 
 import java.net.URLDecoder;
+import java.net.URLEncoder;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,6 +13,11 @@ import java.net.URLDecoder;
 public class DecodeTest {
 
     public static void main(String[] args) {
+
+        System.out.println(URLEncoder.encode("http://alpha.shopping.admin.dp/admin/"));
+
+        System.out.println(URLEncoder.encode("http://alpha.shopping.admin.dp/shopping/admin/searchpromo?type=1&pageNo=1&pageSize=10"));
+
         String href = "dianping://recommendshop?url=http%3A%2F%2Fm.dianping.com%2Fforum%2Fnote%2F7761277%3Fsource%3Dapp%26_utm%3D__2i57%26_p%3Dmain_app_shopping_richbutton%26dp_res%3Dmain_app_shopping_richbutton%26title%3D%25E5%259C%25A3%25E8%25AF%259E%25E8%258A%2582%25E4%25B9%25B0%25E4%25B9%25B0%25E4%25B9%25B0&amp;utm=w_opplatform_app";
         String url = URLDecoder.decode(href.split("url=")[1]);
         String result = url.split("\\?")[0];
